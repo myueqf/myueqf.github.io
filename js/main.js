967f0850eaba52c171f211756ad4ff4cbb05f2b0
myueqf <105153381+myueqf@users.noreply.github.com>
2023-05-14 10:00:26 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -260,29 +260,29 @@ $("#telegram").mouseover(function () {
 });
 
 //自动变灰
-let myDate = new Date;
-let mon = myDate.getMonth() + 1;
-let date = myDate.getDate();
-let days = ['4.4', '5.12', '7.7', '9.9', '9.18', '12.13'];
-for (let day of days) {
-    let d = day.split('.');
-    if (mon == d[0] && date == d[1]) {
-        document.write(
-            '<style>html{-webkit-filter:grayscale(100%);-moz-filter:grayscale(100%);-ms-filter:grayscale(100%);-o-filter:grayscale(100%);filter:progid:DXImageTransform.Microsoft.BasicImage(grayscale=1);_filter:none}</style>'
-        );
-        $("#change").html("Silence&nbsp;in&nbsp;silence");
-        $("#change1").html("今天是中国国家纪念日，全站已切换为黑白模式");
-        window.addEventListener('load', function () {
-            setTimeout(function () {
-                iziToast.show({
-                    timeout: 14000,
-                    icon: "fa-solid fa-clock",
-                    message: '今天是中国国家纪念日'
-                });
-            }, 3800);
-        }, false);
-    }
-}
+//let myDate = new Date;
+//let mon = myDate.getMonth() + 1;
+//let date = myDate.getDate();
+//let days = ['4.4', '5.12', '7.7', '9.9', '9.18', '12.13'];
+//for (let day of days) {
+//    let d = day.split('.');
+//    if (mon == d[0] && date == d[1]) {
+//        document.write(
+//            '<style>html{-webkit-filter:grayscale(100%);-moz-filter:grayscale(100%);-ms-filter:grayscale(100%);-o-filter:grayscale(100%);filter:progid:DXImageTransform.Microsoft.BasicImage(grayscale=1);_filter:none}</style>'
+//        );
+//        $("#change").html("Silence&nbsp;in&nbsp;silence");
+//        $("#change1").html("今天是中国国家纪念日，全站已切换为黑白模式");
+//        window.addEventListener('load', function () {
+//            setTimeout(function () {
+//                iziToast.show({
+//                    timeout: 14000,
+//                    icon: "fa-solid fa-clock",
+//                    message: '今天是中国国家纪念日'
+//                });
+//            }, 3800);
+//        }, false);
+//    }
+//}
 
 //更多页面切换
 let shoemore = false;
