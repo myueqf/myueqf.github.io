b33ada14369c16c3256fed799eba570d4f920f0a
myueqf <myueqf@gmail.com>
2025-08-01 21:41:10 +0800

    雪狐解忧

---
+++ ebook/js/live2d.config.js
@@ -0,0 +1,15 @@
+//////////////////////////////
+// ======== live2d ======== //
+//////////////////////////////
+OML2D.loadOml2d({
+  models: [{
+    /* 雪狐解忧 */
+    path: "../js/live2d/models/150080_ftq_bh/150080_ftq_bh.model.json",
+    position: [-10, 05]
+  }],
+  mobileDisplay: false,  // 不允许在手机端显示
+  dockedPosition: "right",
+  menus: { disable: true },
+  tips: { disable: true },
+  statusBar: { disable: true }
+});
