a350bce38c9270dd6bf9beccb53a24759a1239fa
myueqf <myueqf@gmail.com>
2026-01-12 00:25:15 +0800

    更改下载图标～

--- ebook/js/app.js
+++ ebook/js/app.js
@@ -38,7 +38,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 const downloadLink = document.createElement('a');
                 downloadLink.href = book.url;
-                downloadLink.textContent = '下载';
+                //downloadLink.textContent = '下载';
+                downloadLink.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+                  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+                </svg>`;
                 downloadLink.download = `${book.name} 作者：${book.author}.txt`; // 设置下载文件名
 
                 bookItem.appendChild(bookName);
