c4ddbc4764f1c089f3c8f576704f8370b8b15124
myueqf <myueqf@gmail.com>
2026-01-14 14:14:31 +0800

    ^-^

--- js/music.js
+++ js/music.js
@@ -3,7 +3,7 @@
 ///////////////////////////////
 
 $.ajax({
-    url: "https://api.allorigins.win/raw?url=https://github.com/myueqf/music/releases/latest/download/playList.txt",
+    url: "https://corsproxy.io/https://github.com/myueqf/music/releases/latest/download/playList.txt",
     type: "GET",
     dataType: "text",
     success: function (data) {
