025464a9a919915a6cb2a276d562998154ad319a
myueqf <myueqf@gmail.com>
2026-02-22 21:50:53 +0800

    test

--- ebook/script/all.js
+++ ebook/script/all.js
@@ -1,1 +1,8 @@
-window.addExtraBooks(['js/booklist.json','js/booklist1.json']); 
+window.addExtraBooks(['js/booklist.json','js/booklist1.json']);
+(function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const paramValue = urlParams.get('list');
+    if (paramValue === 'all') {
+        alert(`awa`);
+    }
+})();
