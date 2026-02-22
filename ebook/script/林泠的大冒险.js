0100c3c8df80a7f755345b1e59e1d846e06baff2
myueqf <myueqf@gmail.com>
2026-02-22 20:21:42 +0800

    ^-^

--- ebook/script/林泠的大冒险.js
+++ ebook/script/林泠的大冒险.js
@@ -1,3 +1,4 @@
-window.addExtraBooks(['js/林泠的大冒险.json']);
+import('./all.js');
+window.addExtraXwX(['list/林泠的大冒险.txt']);
 document.title = "甜饼贩卖机";
 document.querySelector('h1').innerText = '书单「林泠的大冒险」 | 阿洇の小说站～';
