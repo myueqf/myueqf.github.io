670ab8685b15dd75fcb08dfa30eb2fbf03dace10
myueqf <myueqf@gmail.com>
2026-02-23 19:07:30 +0800

    ^-^

--- ebook/script/all.js
+++ ebook/script/all.js
@@ -1,1 +1,8 @@
 window.addExtraBooks(['js/booklist.json','js/booklist1.json']);
+(function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const paramValue = urlParams.get('list');
+    if (paramValue === 'all') {
+        window.addExtraXwX(['list/XwX.txt'],'0');
+    }
+})();
