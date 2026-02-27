5f0d12d1a1d19529236431ad93fc3aa8160bf617
myueqf <myueqf@gmail.com>
2026-02-27 21:16:28 +0800

    ^-^

--- ebook/script/XwX.js
+++ ebook/script/XwX.js
@@ -1,5 +1,5 @@
 import('./all.js').then(() => {
-    window.addExtraXwX(['list/XwX.txt']);
+    window.addExtraXwX(['list/小黑屋.txt']);
     document.title = "XwX";
     document.querySelector('h1').innerText = '小黑屋 | 阿洇の小说站～';
 });
