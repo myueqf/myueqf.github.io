8bc99892f5e8d4b71c7871ae74fbe8ec45ecea36
myueqf <myueqf@gmail.com>
2026-02-27 21:45:47 +0800

    ^-^

--- ebook/script/甜饼贩卖机.js
+++ ebook/script/甜饼贩卖机.js
@@ -1,6 +1,10 @@
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 import('./all.js').then(() => {
     window.addExtraXwX(['list/甜饼贩卖机.txt']);
     document.title = "甜饼贩卖机";
     document.querySelector('h1').innerText = '甜饼贩卖机 | 阿洇の小说站～';
-    window.addExtraBooks(['list/list.json']);
+    (async () => {
+        await sleep(1000);
+        window.addExtraBooks(['list/林泠的大冒险.json']);
+    })();
 });
